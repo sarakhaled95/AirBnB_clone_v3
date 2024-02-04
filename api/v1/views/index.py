6f1548d943a81075b9a47c63fb23b3e7347830fe
sarakhaled95 <architectsarakhaled@outlook.com>
2024-02-04 08:17:06 -0800

    2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@
 from api.v1.views import app_views
 import jsonify
 
-@app_views.route("/status")
+@app_views.route('/status')
 def status():
     """ returns a JSON: "status": "OK" """
     return (jsonify({'status': 'OK'}), 200)
